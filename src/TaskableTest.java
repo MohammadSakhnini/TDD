744864984a02d5ebc7fd680debd9162469a8a825
ur-shadow <64754382+ur-shadow@users.noreply.github.com>
2022-04-09 19:58:21 +0200

    Added Test Asserts

--- src/TaskableTest.java
+++ src/TaskableTest.java
@@ -1,17 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TaskableTest {
 
+	Taskable o;
+	String[] trueseq0;
+	String[] falseseq0;
     @BeforeEach
     void setUp() throws Exception {
+    	o = new Task();
+    	trueseq0 = new String[]{ "A" , "B", "C", "D" };
+    	falseseq0 = new String[] {"D" , "C", "B", "A" };
     }
 
     @Test
-    void testIsWellSorted() {
-	fail("Not yet implemented");
+    void testIsWellSortedTrue() {
+    	assertTrue(o.isWellSorted(trueseq0));
+    }
+    
+    @Test
+    void testIsWellSortedFalse() {
+    	assertFalse(o.isWellSorted(falseseq0));
     }
 
 }
