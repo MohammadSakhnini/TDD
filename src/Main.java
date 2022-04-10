9ce60253ebece9adf13cfa112db8f4083bcff785
MohammadSakhnini <49996190+MohammadSakhnini@users.noreply.github.com>
2022-04-10 04:24:29 +0200

    isWellSorted Fixed

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-	var s = new String[][] { {"A","C" }, { "C", "D" }, {"B", "C"} };
-	var task = new Task(s);
+	var prio = new String[][] { { "A", "B" }, { "B", "C" }};
+	var task = new Task(prio);
 	
-        String[] seq1 = new String[] { "A", "B", "C", "D"};
-        String[] seq2 = new String[] { "A", "B", "D", "C" };
-        String[] seq3 = new String[] { "A", "B", "A", "D", "C" };
-        String[] seq4 = new String[] { "A", "A", "C", "D"};
-        String[] seq5 = new String[] { "B", "A", "C", "D"};
-        System.out.println(task.isWellSorted(seq1));
-        System.out.println(task.isWellSorted(seq2));
-        System.out.println(task.isWellSorted(seq3));
-        System.out.println(task.isWellSorted(seq4));
-        System.out.println(task.isWellSorted(seq5));
-
+	String[] seq1 = new String[] { "A", "B", "C", "D" };
+	String[] seq2 = new String[] { "A", "B", "D", "C" };
+	String[] seq3 = new String[] { "A", "B", "A", "D", "C" };
+	String[] seq4 = new String[] { "A", "A", "C", "D" };
+	String[] seq5 = new String[] { "B", "A", "C", "D" };
+	String[] seq6 = new String[] { "C", "A"};
 	
+	System.out.println(task.isWellSorted(seq1));
+	System.out.println(task.isWellSorted(seq2));
+	System.out.println(task.isWellSorted(seq3));
+	System.out.println(task.isWellSorted(seq4));
+	System.out.println(task.isWellSorted(seq5));
+	System.out.println(task.isWellSorted(seq6));
     }
+
+    
 }
--- src/Task.java
+++ src/Task.java
@@ -1,41 +1,93 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Task implements Taskable {
-    private String[][] prioirty;
+    private String[][] prio;
 
-    public Task(String[][] prioirty) {
-	this.prioirty = prioirty;
+    public Task(String[][] prio) {
+	this.prio = prio;
+    }
+    
+    public boolean isWellSorted(String[] seq) {
+	var sequence = Arrays.asList(seq);
+	var map = initMap(prio);
+	fillMap(map, prio);
+	for (int i = 0; i < sequence.size(); i++) {
+	    for (var entry : map.entrySet()) {
+		var key = entry.getKey();
+		var value = entry.getValue();
+		var itr = value.iterator();
+		if (sequence.get(i) == key) {
+		    if (value.isEmpty()) {
+			removeValueFromMap(map, key);
+			continue;
+		    } else {
+			for (String j : sequence.subList(0, i+1)) {
+			    while(itr.hasNext()){
+				var t = itr.next();
+				if (t == j) {
+				    itr.remove();
+				    removeValueFromMap(map, t);
+				    continue;
+				} else {
+				    return false;
+				}
+			    }
+			}
+		    }
+		}
+	    }
+	}
+	return true;
     }
 
-    public boolean isWellSorted(String[] sequence)
-    {
-        for (int iPrio = 0; iPrio < prioirty.length; iPrio++)
-        {
-            for (int jPrio = prioirty[iPrio].length - 1; jPrio >= 1; jPrio--)
-            {
-                for (int i = sequence.length - 1; i >= 0; i--)
-                {
-                    if (sequence[i] == prioirty[iPrio][jPrio])
-                    {
-                        if (Contains(sequence, i + 1, prioirty[iPrio][jPrio - 1]))
-                        {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
+    private static void removeValueFromMap(HashMap<String, List<String>> map, String valueToRemove) {
+	for (var entry : map.entrySet()) {
+	    var key = entry.getKey();
+	    var value = entry.getValue();
+	    var itr = value.iterator();
+	    while(itr.hasNext()){
+		var i = itr.next();
+		if (i == valueToRemove) {
+		    itr.remove();
+		}
+	    }
+	}
 
-        return true;
     }
 
-    private boolean Contains(String[] sequence, int from, String value)
-    {
-        for (int i = from; i < sequence.length; i++)
-        {
-            if (sequence[i] == value)
-                return true;
-        }
+    private static HashMap<String, List<String>> initMap(String[][] prios) {
+	String[] temp = flatten(prios);
+	Set<String> set = new HashSet<>(Arrays.asList(temp));
+	HashMap<String, List<String>> results = new HashMap<>();
+	for (String i : set) {
+	    results.put(i, new ArrayList<String>());
+	}
+	return results;
+    }
 
-        return false;
+    private static void fillMap(HashMap<String, List<String>> map, String[][] prio) {
+	for (var seq : prio) {
+	    var letter = seq[seq.length - 1];
+	    var rest = Arrays.copyOfRange(seq, 0, seq.length - 1);
+	    for (var value : rest) {
+		map.get(letter).add(value);
+	    }
+	}
+    }
+
+    private static String[] flatten(String[][] arr) {
+	var result = new ArrayList<String>();
+	for (int i = 0; i < arr.length; i++) {
+	    for (int j = 0; j < arr[i].length; j++) {
+		result.add(arr[i][j]);
+	    }
+	}
+	return result.toArray(new String[0]);
     }
 
 }
